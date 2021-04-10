7a775e136a46ae9d425a1947ee957a3db25639a8
Reese Avery Kling <raveryk@gmail.com>
2021-04-09 19:02:46 -0500

    created input fields and submit button to append info to the DOM. Also am able to store data and calculate monthly costs.

---
+++ client.js
@@ -0,0 +1,66 @@
+$(document).ready(onReady);
+
+
+function onReady() {
+console.log('JS');
+
+$('#submit-button').on('click', addInputs);
+
+
+};
+
+
+let salaryTotal = 0;
+
+
+function addInputs() {
+
+
+$('.table-row').empty();    
+
+
+console.log('Adding Inputs');
+
+let employee = {
+    firstName: $('#in-first-name').val(),
+    lastName: $('#in-last-name').val(),
+    idNum: $('#in-id-num').val(),
+    jobTitle: $('#in-job-title').val(),
+    salary: $('#in-salary').val()
+}
+
+$('.display-inputs').append(`<tr>
+    <td>${employee.firstName}</td>
+    <td>${employee.lastName}</td>
+    <td>${employee.idNum}</td>
+    <td>${employee.jobTitle}</td>
+    <td>${employee.salary}</td>
+</tr>`);
+
+let salary = (Number(employee.salary));
+salaryTotal += salary;
+
+
+clearInputs();
+monthCosts();
+
+console.log('Total Annual Salary: ', salaryTotal);
+    
+}
+
+function clearInputs() {
+    $('#in-first-name').val('');
+    $('#in-last-name').val('');
+    $('#in-id-num').val('');
+    $('#in-job-title').val('');
+    $('#in-salary').val('');
+}
+
+function monthCosts() {
+
+let monthCost = salaryTotal / 12;
+
+// $('#month-cost-out').
+$('#month-cost-out').empty().append(monthCost);
+
+}
